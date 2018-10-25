a883aa92a6e01c12bdb93199819edd6ecd88dd5e
Pawel Kubiak <pawel.kubiak@ormlondon.com>
2018-10-25 20:37:21 +0200

    Loop examples - fix bug

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         LoopTest loopTest = new LoopTest(scanner);
-        loopTest.Test();
+        loopTest.test();
     }
 }
