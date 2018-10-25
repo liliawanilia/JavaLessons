6f4d955fe07b44aa229c32cf9240e72860b4be58
Pawel Kubiak <pawel.kubiak@ormlondon.com>
2018-10-25 20:20:42 +0200

    Loop examples - main() improvments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,68 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         LoopTest loopTest = new LoopTest();
-        loopTest.PrintColumn(5);
-        loopTest.PrintRow(5);
-        loopTest.PrintSquare(5);
-        loopTest.PrintLeftTriangle(5);
-        loopTest.PrintRightTriangle(5);
-        loopTest.PrintDiagonalPattern(5);
-        loopTest.PrintDiagonalSecondPattern(5);
-        loopTest.PrintPowers(5);
+
+        System.out.println("Program will print stars pattern. Please select version:" +
+                "\n1 - column" +
+                "\n2 - row" +
+                "\n3 - square" +
+                "\n4 - triangle v1" +
+                "\n5 - triangle v2" +
+                "\n6 - diagonal v1" +
+                "\n7 - diagonal v2" +
+                "\n8 - power matrix");
+
+        int selectedPattern = scanner.nextInt();
+        switch (selectedPattern) {
+            case 1:
+            {
+                loopTest.PrintColumn(5);
+                break;
+            }
+            case 2:
+            {
+                loopTest.PrintRow(5);
+                break;
+            }
+            case 3:
+            {
+                loopTest.PrintSquare(5);
+                break;
+            }
+            case 4:
+            {
+                loopTest.PrintLeftTriangle(5);
+                break;
+            }
+            case 5:
+            {
+                loopTest.PrintRightTriangle(5);
+                break;
+            }
+            case 6:
+            {
+                loopTest.PrintDiagonalPattern(5);
+                break;
+            }
+            case 7:
+            {
+                loopTest.PrintDiagonalSecondPattern(5);
+                break;
+            }
+            case 8:
+            {
+                loopTest.PrintPowers(5);
+                break;
+            }
+            default: {
+                System.out.println("You selected wrong value!");
+            }
+        }
     }
 }
